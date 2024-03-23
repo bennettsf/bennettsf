c7aec3b852ae512530336e5b43f7f6345e8d6edc
Bennett Fife <33855430+bennettsf@users.noreply.github.com>
2024-03-22 20:16:02 -0700

    2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bennettsf/"
+  base: "/bennettsf/",
+  server: {
+    host: true,
+    port: 22,
+  }
 })
